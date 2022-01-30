0841f6d7104c1fda401956795e1449e320751dce
somu4git <somu.guntur@gmail.com>
2022-01-30 15:01:07 -0700

    Update bootstrap.sh
    updated 01/30

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,9 +31,10 @@ sysctl --system >/dev/null 2>&1
 
 echo "[TASK 5] Install containerd runtime"
 apt update -qq >/dev/null 2>&1
-apt install -qq -y containerd apt-transport-https >/dev/null 2>&1
+apt install -qq -y containerd apt-transport-https ca-certificates curl >/dev/null 2>&1
 mkdir /etc/containerd
 containerd config default > /etc/containerd/config.toml
+sed -i -e 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
 systemctl restart containerd
 systemctl enable containerd >/dev/null 2>&1
 
@@ -43,6 +44,7 @@ apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/
 
 echo "[TASK 7] Install Kubernetes components (kubeadm, kubelet and kubectl)"
 apt install -qq -y kubeadm=1.22.0-00 kubelet=1.22.0-00 kubectl=1.22.0-00 >/dev/null 2>&1
+apt-mark hold kubelet kubeadm kubectl
 
 echo "[TASK 8] Enable ssh password authentication"
 sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
@@ -52,12 +54,16 @@ systemctl reload sshd
 echo "[TASK 9] Set root password"
 echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
 echo "export TERM=xterm" >> /etc/bash.bashrc
+# Add NOPASSWD to vagrant user?
+echo vagrant ALL=NOPASSWD:ALL > /etc/sudoers.d/vagrant
+chmod 0440 /etc/sudoers.d/vagrant
 
 echo "[TASK 10] Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
-172.16.16.100   kmaster.example.com     kmaster
-172.16.16.101   kworker1.example.com    kworker1
-172.16.16.102   kworker2.example.com    kworker2
+192.168.212.100   kmaster.example.com     kmaster
+192.168.212.101   kworker1.example.com    kworker1
+192.168.212.102   kworker2.example.com    kworker2
 EOF
 
-
+echo "[TASK 11] Add Kubectl autocomplete"
+echo "source <(kubectl completion bash)" >> ~/.bashrc
